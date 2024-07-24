90802aa5f235c51969544bdb2cd1b99175412055
Chris Bizon <bizon@renci.org>
2024-07-24 13:50:27 -0400

    copy data

---
+++ copy_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#pod="interactive-torch-geometric"
+pod="rotate-robokop-jz7cr"
+dirname="CGD"
+kubectl exec --context 'bizon@sterling' -n bizon $pod -- mkdir -p /workspace/data/robokop/$dirname/raw
+kubectl cp --context 'bizon@sterling' -n bizon robokop/$dirname/robo_train.txt $pod:/workspace/data/robokop/$dirname/raw/robo_train.txt
+kubectl cp --context 'bizon@sterling' -n bizon robokop/$dirname/robo_test.txt $pod:/workspace/data/robokop/$dirname/raw/robo_test.txt
+kubectl cp --context 'bizon@sterling' -n bizon robokop/$dirname/robo_val.txt $pod:/workspace/data/robokop/$dirname/raw/robo_valid.txt
