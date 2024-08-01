5e55962ccac5808c6d27a521fa64a4721674f7c4
Chris Bizon <bizon@renci.org>
2024-08-01 09:10:53 -0400

    copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pod="interactive-torch-geometric"
-dirname="CCD"
+dirname="CGGD"
 kubectl exec --context 'bizon@sterling' -n bizon $pod -- mkdir -p /workspace/data/robokop/$dirname/raw
 kubectl cp --context 'bizon@sterling' -n bizon robokop/$dirname/robo_train.txt $pod:/workspace/data/robokop/$dirname/raw/robo_train.txt
 kubectl cp --context 'bizon@sterling' -n bizon robokop/$dirname/robo_test.txt $pod:/workspace/data/robokop/$dirname/raw/robo_test.txt
