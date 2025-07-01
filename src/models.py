89948cf4a9b20e30ce0265dffacecc10b8d38978
james0032 <jchung@renci.org>
2025-07-01 09:31:25 -0400

    Add conve test

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import Tuple
+from torch import Tensor
+
 from torch_geometric.loader import LinkNeighborLoader
 from torch_geometric.data import Data
 from torch.utils.data import DataLoader, Dataset, TensorDataset
@@ -61,6 +64,52 @@ class ConvE(torch.nn.Module):
         #return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
         dataset = TripleDataset(head_index, rel_type, tail_index)
         return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=True)
+    
+    @torch.no_grad()
+    def test(
+        self,
+        head_index: Tensor,
+        rel_type: Tensor,
+        tail_index: Tensor,
+        batch_size: int,
+        k: int = 10,
+        log: bool = True,
+    ) -> Tuple[float, float, float]:
+        r"""Evaluates the model quality by computing Mean Rank, MRR and
+        Hits@:math:`k` across all possible tail entities.
+
+        Args:
+            head_index (torch.Tensor): The head indices.
+            rel_type (torch.Tensor): The relation type.
+            tail_index (torch.Tensor): The tail indices.
+            batch_size (int): The batch size to use for evaluating.
+            k (int, optional): The :math:`k` in Hits @ :math:`k`.
+                (default: :obj:`10`)
+            log (bool, optional): If set to :obj:`False`, will not print a
+                progress bar to the console. (default: :obj:`True`)
+        """
+        arange = range(head_index.numel())
+        #arange = tqdm(arange) if log else arange
+
+        mean_ranks, reciprocal_ranks, hits_at_k = [], [], []
+        for i in arange:
+            h, r, t = head_index[i], rel_type[i], tail_index[i]
+
+            scores = []
+            tail_indices = torch.arange(self.num_nodes, device=t.device)
+            for ts in tail_indices.split(batch_size):
+                scores.append(self(h.expand_as(ts), r.expand_as(ts), ts))
+            rank = int((torch.cat(scores).argsort(
+                descending=True) == t).nonzero().view(-1))
+            mean_ranks.append(rank)
+            reciprocal_ranks.append(1 / (rank + 1))
+            hits_at_k.append(rank < k)
+
+        mean_rank = float(torch.tensor(mean_ranks, dtype=torch.float).mean())
+        mrr = float(torch.tensor(reciprocal_ranks, dtype=torch.float).mean())
+        hits_at_k = int(torch.tensor(hits_at_k).sum()) / len(hits_at_k)
+
+        return mean_rank, mrr, hits_at_k
     
 class TripleDataset(Dataset):
     def __init__(self, head_index, rel_type, tail_index):
