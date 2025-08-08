c5d458fce526302246139aa06e64c585e949640c
james0032 <jchung@renci.org>
2025-08-08 09:28:03 -0400

    upadte replace emb to all polars lazy mode

--- src/replace_emb.py
+++ src/replace_emb.py
@@ -8,40 +8,42 @@ import os
 
 BASE_PATH = "/workspace/data/robokop/rCD"
 
-df = pl.read_parquet("gs://mtrx-us-central1-hub-dev-storage/kedro/data/tests/rotate_esuite/runs/rtx-base-feat3-3ce2b620/datasets/embeddings/feat/nodes_with_embeddings/")
+df = pl.scan_parquet("gs://mtrx-us-central1-hub-dev-storage/kedro/data/tests/rotate_esuite/runs/rtx-base-feat3-3ce2b620/datasets/embeddings/feat/nodes_with_embeddings/")
 print("Begining size of nodes with embeddings", df.shape)
-new = pd.read_csv(os.path.join(BASE_PATH, "projected_entity_embeddings.tsv"), sep='\t', header=None, names=["id", "topo"])
-
-# Step 2: Vectorized string-to-list[f32] parsing
-new["topological_embedding"] = new["topo"].progress_apply(
-    lambda x: np.array(ast.literal_eval(x), dtype=np.float32)
-)
+newpl = pd.scan_csv(os.path.join(BASE_PATH, "projected_entity_embeddings.tsv"), separator='\t', has_header=False)
+newpl = newpl.rename({newpl.columns[0]: "id", newpl.columns[1]: "topological_embedding"})
+## Step 2: Vectorized string-to-list[f32] parsing
+#new["topological_embedding"] = new["topo"].progress_apply(
+#    lambda x: np.array(ast.literal_eval(x), dtype=np.float32)
+#)
+#new.to_csv(os.path.join(BASE_PATH, "formatted_projected_entity_embeddings.tsv"), sep='\t', index=False)
 # Step 3: Convert to Polars and cast (if needed)
-new = pl.from_pandas(new)
-new = new.with_columns(
+#newpl = pl.from_pandas(new)
+newpl = newpl.with_columns(
     pl.col("topological_embedding").cast(pl.List(pl.Float32))
 )
-new = new.drop("topo")
+#newpl = newpl.drop("topo")
 
 # Step 4: df to drop topo and join new to df
 df = df.drop("topological_embedding")
-df = df.join(new, on="id", how="inner")
-print("Final size of nodes with embeddings", df.shape)
+df = df.join(newpl, on="id", how="inner")
+#print("Final size of nodes with embeddings", df.shape)
 
 output_dir = os.path.join(BASE_PATH, "rotate_emb")
 os.makedirs(output_dir, exist_ok=True)
 
 # Split into 200 roughly equal partitions
 num_partitions = 200
-partition_size = len(new) // num_partitions + 1
+row_count = df.select(pl.count()).collect().item()
+partition_size = row_count // num_partitions + 1
 
 for i in range(num_partitions):
     start = i * partition_size
-    end = min((i + 1) * partition_size, len(new))
-    df_slice = df.slice(start, end - start)
+    #end = min((i + 1) * partition_size, len(newpl))
+    df_slice = df.slice(start, partition_size)
     
     # Save each partition
-    df_slice.write_parquet(
+    df_slice.sink_parquet(
         f"{output_dir}/part_{i:05d}.snappy.parquet", 
         compression="snappy"
     )
