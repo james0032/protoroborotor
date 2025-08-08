794576fd3f65226c6d8bcc3b46768f59326be60b
james0032 <jchung@renci.org>
2025-08-08 09:29:57 -0400

    upadte replace emb to all polars lazy mode

--- src/replace_emb.py
+++ src/replace_emb.py
@@ -9,7 +9,9 @@ import os
 BASE_PATH = "/workspace/data/robokop/rCD"
 
 df = pl.scan_parquet("gs://mtrx-us-central1-hub-dev-storage/kedro/data/tests/rotate_esuite/runs/rtx-base-feat3-3ce2b620/datasets/embeddings/feat/nodes_with_embeddings/")
-print("Begining size of nodes with embeddings", df.shape)
+#print("Begining size of nodes with embeddings", df.shape)
+row_count = df.select(pl.count()).collect().item()
+print("df has number of rows", row_count)
 newpl = pd.scan_csv(os.path.join(BASE_PATH, "projected_entity_embeddings.tsv"), separator='\t', has_header=False)
 newpl = newpl.rename({newpl.columns[0]: "id", newpl.columns[1]: "topological_embedding"})
 ## Step 2: Vectorized string-to-list[f32] parsing
@@ -23,7 +25,8 @@ newpl = newpl.with_columns(
     pl.col("topological_embedding").cast(pl.List(pl.Float32))
 )
 #newpl = newpl.drop("topo")
-
+newpl_count = newpl.select(pl.count()).collect().item()
+print("New emb is ready for merge and has number of rows:", newpl_count)
 # Step 4: df to drop topo and join new to df
 df = df.drop("topological_embedding")
 df = df.join(newpl, on="id", how="inner")
@@ -34,7 +37,7 @@ os.makedirs(output_dir, exist_ok=True)
 
 # Split into 200 roughly equal partitions
 num_partitions = 200
-row_count = df.select(pl.count()).collect().item()
+
 partition_size = row_count // num_partitions + 1
 
 for i in range(num_partitions):
