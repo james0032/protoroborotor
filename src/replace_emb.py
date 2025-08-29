d1b49681e577556bdac44616f365e004082576df
james0032 <jchung@renci.org>
2025-08-29 08:26:20 -0400

    update replace_emb

--- src/replace_emb.py
+++ src/replace_emb.py
@@ -11,8 +11,8 @@ DIM = 512
 # Step 1: Read both old emb files
 df = pl.scan_parquet("gs://mtrx-us-central1-hub-dev-storage/kedro/data/tests/emb_replace_robokop_cgd_subgraph/datasets/embeddings/feat/nodes_with_embeddings/")
 #print("Begining size of nodes with embeddings", df.shape)
-df = df.with_columns(pl.col("id").cast(pl.Utf8).str.strip_chars('"'))
-row_count = df.select(pl.len()).collect().row(0)[0]
+#df = df.with_columns(pl.col("id").cast(pl.Utf8).str.strip_chars('"'))
+row_count = df.select(pl.len()).collect().item()
 print("df has number of rows", row_count)
 #dupes = df.group_by("id").agg(pl.len().alias("count")).filter(pl.col("count")>1)
 #print("Duplicated id in matrix pipeline generated embedding file", dupes.collect())
