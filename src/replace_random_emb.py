a3e429ae6a10b385757d7c6f310f095056fdf995
james0032 <jchung@renci.org>
2025-09-12 23:28:44 -0400

    v0.10.1 run for rotatE

--- src/replace_random_emb.py
+++ src/replace_random_emb.py
@@ -10,7 +10,7 @@ import torch
 BASE_PATH = "/workspace/data/robokop/rCD_robokop_emb_predicate_only"
 DIM = 512
 # Step 1: Read both old emb files
-df = pl.scan_parquet("gs:/mtrx-us-central1-hub-dev-storage/kedro/data/tests/emb_replace_robokop_v0_10_1/datasets/embeddings/feat/nodes_with_embeddings/")
+df = pl.scan_parquet("gs://mtrx-us-central1-hub-dev-storage/kedro/data/tests/emb_replace_robokop/datasets/embeddings/feat/nodes_with_embeddings/")
 #print("Begining size of nodes with embeddings", df.shape)
 df = df.with_columns(pl.col("id").cast(pl.Utf8).str.strip_chars('"'))
 row_count = df.select(pl.len()).collect().row(0)[0]
